4d1febf35cb258b9166af898c9713ef10e43da92
CarsenWei <j56wei@edu.uwaterloo.ca>
2018-11-21 16:55:35 -0800

    completed odbc_interface.py

--- xuebadb/dbgeneric/odbc_interface.py
+++ xuebadb/dbgeneric/odbc_interface.py
@@ -2,34 +2,38 @@ import pyodbc
 import pandas
 
 class ODBCInterface:
-    def __init__(self,server,username,password,dbname):
+    def __init__(self, server, username, password, dbname):
         self.server = server
         self.username = username
         self.password = password
         self.dbname = dbname
-    def __connect():  #private
+        
+    def __connect(self): #private
         try:
             self.cnx = pyodbc.connect("""DRIVER={ODBC Driver 17 for SQL Server};
-                        SERVER="""+str(self.server)+""";
-                        DATABASE="""+str(dbname)+""";
-                        UID="""+str(username)+""";
-                        PWD="""+str(password))
+                                      SERVER="""+str(self.server)+""";
+                                      DATABASE="""+str(self.dbname)+""";
+                                      UID="""+str(self.username)+""";
+                                      PWD="""+str(self.password))
         except pyodbc.Error as err:  
             print("Error when connecting to the database.")
             print(err)
             self.cnx.close()
+            
     def select(self, query):
         try:
             self.__connect()
-            cursor = cnx.cursor()
+            cursor = self.cnx.cursor()
             cursor.execute(query) 
-            output=[]
+            output = []
             for row in cursor:
-                output.append(row)
+                inner_list = []
+                for val in row:
+                    inner_list.append(str(val).strip())  #remove trailing white space to create dataframe
+                output.append(inner_list)
             cursor.close()
-            output=pandas.DataFrame(output)
-            return output
-        except pyodbc.Error as err:  
-            print(err)
-            
-test=ODBCInterface("sql04.ok.ubc.ca","rlawrenc","test","workson")
+            return pandas.DataFrame(output)
+        except:  
+            print("Unable to run SELECT query")
+        finally:
+            self.cnx.close()
