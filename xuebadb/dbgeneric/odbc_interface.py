4dcd28e7c9a3a53e2d9a265a784d2684d4ae5932
CarsenWei <j56wei@edu.uwaterloo.ca>
2018-11-21 16:33:09 -0800

    odbc

--- xuebadb/dbgeneric/odbc_interface.py
+++ xuebadb/dbgeneric/odbc_interface.py
@@ -1,7 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 21 14:10:14 2018
+import pyodbc
+import pandas
 
-@author: Vaghul
-"""
-
+class ODBCInterface:
+    def __init__(self,server,username,password,dbname):
+        self.server = server
+        self.username = username
+        self.password = password
+        self.dbname = dbname
+    def __connect():  #private
+        try:
+            self.cnx = pyodbc.connect("""DRIVER={ODBC Driver 17 for SQL Server};
+                        SERVER="""+str(self.server)+""";
+                        DATABASE="""+str(dbname)+""";
+                        UID="""+str(username)+""";
+                        PWD="""+str(password))
+        except pyodbc.Error as err:  
+            print("Error when connecting to the database.")
+            print(err)
+            self.cnx.close()
+    def select(self, query):
+        try:
+            self.__connect()
+            cursor = cnx.cursor()
+            cursor.execute(query) 
+            output=[]
+            for row in cursor:
+                output.append(row)
+            cursor.close()
+            output=pandas.DataFrame(output)
+            return output
+        except pyodbc.Error as err:  
+            print(err)
+            
+test=ODBCInterface("sql04.ok.ubc.ca","rlawrenc","test","workson")
