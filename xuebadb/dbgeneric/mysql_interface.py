4bae66166b626d012bd96bc4e9d3c41b0971277b
Vaghul Aditya Balaji <vaghulb1992@gmail.com>
2018-11-21 16:03:50 -0800

    Adding MySQL interface for the sub-package

--- xuebadb/dbgeneric/mysql_interface.py
+++ xuebadb/dbgeneric/mysql_interface.py
@@ -1,7 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 21 14:09:59 2018
+import mysql.connector
 
-@author: Vaghul
-"""
-
+class MySQLInterface:    
+    def __init__(self, server, username, password, dbname):
+        self.server = server
+        self.username = username
+        self.password = password
+        self.dbname = dbname
+        
+    def __connect(self):        
+        try:
+            self.cnx = mysql.connector.connect(user=self.username, password=self.password, host=self.server, database=self.dbname)
+        except mysql.connector.Error as err:
+            print(err)
+            self.cnx.close()
+            
+    def select(self, query):
+        try:
+            self.__connect()
+            output = []
+            cursor = self.cnx.cursor()
+            cursor.execute(query)
+            for row in cursor:
+                output.append(row)
+            cursor.close()
+            return output
+        except:
+            print("Unable to run the SELECT query")
+        finally:
+            self.cnx.close()
+            
