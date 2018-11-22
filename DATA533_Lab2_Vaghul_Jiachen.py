84e6aaebdecf464a4ad8d699f8b8194724236920
Vaghul Aditya Balaji <vaghulb1992@gmail.com>
2018-11-21 17:37:11 -0800

    Adding test file

--- DATA533_Lab2_Vaghul_Jiachen.py
+++ DATA533_Lab2_Vaghul_Jiachen.py
@@ -1,2 +1,8 @@
-import xuebadb
+import xuebadb.dbgeneric.mysql_interface as mysql
+import xuebadb.dbgeneric.odbc_interface as odbc
 
+mysql_con = mysql.MySQLInterface('cosc304.ok.ubc.ca', 'vbalaji', '10796456', 'WorksOn')
+mysql_res = mysql_con.select("select * from dept")
+
+odbc_con = odbc.ODBCInterface("sql04.ok.ubc.ca", "rlawrenc", "test", "workson")
+odbc_res = odbc_con.select("select * from dept")
