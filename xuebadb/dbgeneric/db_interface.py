3a0e67f87846dc1a86a26c25235c9dbbe19fdf5e
CarsenWei <j56wei@edu.uwaterloo.ca>
2018-11-21 18:06:13 -0800

    db_interface.py

--- xuebadb/dbgeneric/db_interface.py
+++ xuebadb/dbgeneric/db_interface.py
@@ -1,7 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 21 14:08:35 2018
-
-@author: Vaghul
-"""
-
+from mysql_interface import MySQLInterface
+from odbc_interface import ODBCInterface
+class DBInterface(ODBCInterface, MySQLInterface):   #inherit
+    def __init__(self,dbtype,server,username,password,dbname):
+        dbtypelist=['sql_server','mysql']
+        if dbtype not in dbtypelist:
+            raise Exception("The argument \'dbtype\' must be in the list {}".format(dbtypelist) )
+        if dbtype=='sql_server':
+            ODBCInterface.__init__(self,server,username,password,dbname)
+            self.dbtype = dbtype
+        elif dbtype=='mysql':
+            MySQLInterface.__init__(self, server, username, password, dbname)
+            self.dbtype = dbtype
+    def querySelect(self,query):
+        if self.dbtype=='sql_server':
+            ODBCInterface.select(self,query)
+        if self.dbtype=='mysql':
+            MySQLInterface.select(self,query)
