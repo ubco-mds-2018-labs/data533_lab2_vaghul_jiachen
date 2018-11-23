c3572dc1179040b474a220986b5718bcce2855db
Jiachen Wei <42905348+CarsenWei@users.noreply.github.com>
2018-11-22 16:00:37 -0800

    Update stats.py

--- xuebadb/dfanalysis/stats.py
+++ xuebadb/dfanalysis/stats.py
@@ -1,7 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 21 14:21:58 2018
-
-@author: Vaghul
-"""
-
+import pandas
+import matplotlib.pyplot as plt
+def dfSummary(data):
+    try:
+        return data.describe()  #statistical summary
+    except:
+        print("Unable to provide statistical summary.",err)
+def colBoxPlot(data):
+    for col in range(0,len(data.columns)):
+        try:
+            floatdata=data[col].astype(float) #try to convert each column to float and create box plot
+        except ValueError as err:
+            print("Unable to create box plot for column",col,'\t',err)     
+        else:
+            floatdata.plot.box()
